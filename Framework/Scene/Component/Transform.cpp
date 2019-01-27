f5258d46028aa2e2edab4314347340fcb5c99fda
Jin Woo Lee <rladudfk456@naver.com>
2019-01-28 06:37:03 +0900

    190124 Transform

--- Framework/Scene/Component/Transform.cpp
+++ Framework/Scene/Component/Transform.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "Transform.h"
 
-Transform::Transform(Context * context) : context(context), localScale(1, 1, 1), localRotation(0, 0, 0), localPosition(0, 0, 0)
+Transform::Transform(Context * context) : context(context), localScale(1.0f), localRotation(0.0f), localPosition(0.0f), parent(nullptr)
 {
 	local.SetIdentity();
 	world.SetIdentity();
@@ -9,4 +9,94 @@ Transform::Transform(Context * context) : context(context), localScale(1, 1, 1),
 
 Transform::~Transform()
 {
+}
+
+const Matrix Transform::GetWorldRotationMatrix()
+{
+	Vector3 scale = GetScale();
+
+	bool bCheck = false;
+	bCheck |= scale.x == 0.0f;
+	bCheck |= scale.y == 0.0f;
+	bCheck |= scale.z == 0.0f;
+
+	if (bCheck) return Matrix();
+
+	float factorX = 1.0f / scale.x;
+	float factorY = 1.0f / scale.y;
+	float factorZ = 1.0f / scale.z;
+
+	return Matrix(
+		world._11 * factorX, world._12 * factorX, world._13 * factorX, world._14 * factorX,
+		world._21 * factorY, world._22 * factorY, world._23 * factorY, world._24 * factorY,
+		world._31 * factorZ, world._32 * factorZ, world._33 * factorZ, world._34 * factorZ,
+		0.0f, 0.0f, 0.0f, 1.0f
+	);
+}
+
+void Transform::SetLocalScale(const Vector3 & localScale)
+{
+	if (this->localScale == localScale) return;
+	this->localScale = localScale;
+	UpdateTransform();
+}
+
+void Transform::SetLocalRotation(const Vector3 & localRotation)
+{
+	if (this->localRotation == localRotation) return;
+	this->localRotation = localRotation;
+	UpdateTransform();
+}
+
+void Transform::SetLocalPosition(const Vector3 & localPosition)
+{
+	if (this->localPosition == localPosition) return;
+	this->localPosition = localPosition;
+	UpdateTransform();
+}
+
+void Transform::SetScale(const Vector3 & scale)
+{
+	if (GetScale() == scale) return;
+	if (HasParent()) {
+		Vector3 parentScale = parent->GetScale();
+		Vector3 s = scale / parentScale;
+		SetLocalScale(s);
+	}
+	else SetLocalScale(scale);
+}
+
+void Transform::SetRotation(const Vector3 & rotation)
+{
+	if (GetRotation() == rotation) return;
+	if (HasParent()) {
+		Matrix inv = Matrix::Inverse(parent->GetWorldRotationMatrix()); //역행렬 곱하면 해당 행렬이 단위행렬로 변함
+		SetLocalRotation(Vector3::TransformNormal(rotation, inv)); //회전은 방향이므로 normal
+	}
+	else SetLocalRotation(rotation);
+}
+
+void Transform::SetPosition(const Vector3 & position)
+{
+	if (GetPosition() == position) return;
+	if (HasParent()) {
+		Matrix inv = Matrix::Inverse(parent->GetWorldMatrix()); //역행렬 곱하면 해당 행렬이 단위행렬로 변함
+		SetLocalPosition(Vector3::TransformCoord(position, inv));
+	}
+	else SetLocalPosition(position);
+}
+
+void Transform::UpdateTransform()
+{
+	Matrix S = Matrix::Scaling(localScale);
+	Matrix R = Matrix::RotationYawPitchRoll(localRotation);
+	Matrix T = Matrix::Translation(localPosition);
+
+	local = S * R * T; //자식만의 world,  world->parent행렬을 곱한 것
+
+	if (HasParent) world = local * parent->GetWorldMatrix();
+	else world = local;
+
+	//참조가 안붙어도 됨. 동적이므로. 정적이면 붙여야함. 우리는 childs의 복사를 만들지 않고 그 주소 그대로 쓰기 위해 &을 붙임
+	for (auto &child : childs) child->UpdateTransform(); //자식이 없으면 vector가 0이므로 확인할 필요 없음
 }
--- Framework/Scene/Component/Transform.h
+++ Framework/Scene/Component/Transform.h
@@ -6,6 +6,30 @@ public:
 	Transform(class Context *context);
 	virtual ~Transform();
 
+	const Matrix &GetLocalMatrix() const { return local; } //&자기자신의 공간이 나간다. 무조건 class에 포함되어 있는 변수여야 함
+	const Matrix &GetWorldMatrix() const { return world; }
+	const Matrix GetWorldRotationMatrix();
+
+	const Vector3 &GetLocalScale() const { return localScale; } 
+	const Vector3 &GetLocalRotation() const { return localRotation; }
+	const Vector3 &GetLocalPosition() const { return localPosition; }
+
+	const Vector3 GetScale() { return world.GetScale(); } //world, 자기가 부모라면 local과 같음. 자식이라면 다름.
+	const Vector3 GetRotation() { return world.GetRotation(); } 
+	const Vector3 GetPosition() { return world.GetTranslation(); }
+
+	void SetLocalScale(const Vector3 &localScale);
+	void SetLocalRotation(const Vector3 &localRotation);
+	void SetLocalPosition(const Vector3 &localPosition);
+
+	void SetScale(const Vector3 &scale); //world, world가 곱해진 scale이 들어옴, 자식의 scale만 늘리면 parent를 침범할 수 있음. parent의 변화까지 적용된 scale이 필요
+	void SetRotation(const Vector3 &rotation);
+	void SetPosition(const Vector3 &position);
+
+	const bool HasParent() const { return parent ? true : false; } //class가 변수를 가지고 있지 않음. 지역변수이기 때문에 만들고 나가면 사라지는데 그 공간을 참조하게 되므로 &안붙음 
+	const bool HasChild() const { return !childs.empty(); } //empty->비어있으면 true
+	void UpdateTransform();
+
 private:
 	class Context *context;
 
@@ -13,6 +37,11 @@ private:
 	Vector3 localRotation;
 	Vector3 localPosition;
 
+	//local은 자기 공간기준으로 떨어져 있는 팔.
+	//parent를 적용하면 몸에 팔이 붙고 붙은 위치(world)
 	Matrix local;
 	Matrix world;
+
+	Transform *parent; //부모 자식 관계를 설정해 부모를 따라 자식도 움직이도록 함
+	vector<Transform*> childs;
 };
